4dfdaf160135178adced54f3d555118214e2aed4
parthparakh26 <86876173+parthparakh26@users.noreply.github.com>
2023-02-12 06:13:48 +0530

    CropsPredicted

--- crop prediction/src/components/CropPredictor/CropPredictor.js
+++ crop prediction/src/components/CropPredictor/CropPredictor.js
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import "./CropPredictor.css";
 import logo from '../../assets/Icon.png'
 import axios from 'axios';
 
 const App = () => {
   const [location, setLocation] = useState({ district : "" , state : ""});
+  const [weather, setWeather] = useState({ temperature : "" , humidity : ""});
   const [seasonCrop, setSeasonCrop] = useState("");
+  const [data, setData] = useState({});
 
   async function onSubmit(e) {
     e.preventDefault(); 
 
-    const district = location.district;
-    const state = location.state;
-    const season = seasonCrop
-    const formData = {state,district,season}
+    const District = location.district;
+    const State = location.state;
+    const Season = seasonCrop
+    const formData = {District,State,Season}
 
-    axios.post('http://127.0.0.1:8000/api/messages/', formData)
+    axios.post('http://localhost:8000/api/messages/', formData)
       .then(response => {
         console.log(response);
       })
@@ -24,28 +26,45 @@ const App = () => {
       });
 
       console.log(formData);
+
+      const fetchData = async () => {
+        const result = await axios.get('http://127.0.0.1:8000/predictions/predict/');
+        setData(result.data);
+      };
+
+      fetchData();
 }
 
   const handleSeasonCropChange = (event) => {
     setSeasonCrop(event.target.value);
   };
 
+  let lat,lon,apiKey;
+
+  navigator.geolocation.getCurrentPosition((position) => {
+     lat = position.coords.latitude;
+     lon = position.coords.longitude;
+     apiKey = "pk.8ca69bbdbb997de3f1933c0be42fd71a";
+    });  
+
   const getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const apiKey = "pk.8ca69bbdbb997de3f1933c0be42fd71a";
-      
-        fetch(
-          `https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${lat}&lon=${lon}&format=json`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            setLocation({ district: data.address.state_district, state: data.address.state });
-          });
-      });
-      
-  };
+    fetch(`https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${lat}&lon=${lon}&format=json`)
+        .then((response) => response.json())
+        .then((data) => {
+        setLocation({ district: data.address.state_district, state: data.address.state });
+        });
+    };
+
+
+  const getCurrentWeather = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=90d6875860594f5f913215234231102&q=${lat},${lon}`)
+        .then((response) => response.json())
+        .then((data) => {
+        setWeather({ temperature: data.current.temp_c, humidity: data.current.humidity });
+    });
+};
+
+
 
   return (
     <div>
@@ -61,7 +80,16 @@ const App = () => {
         <label htmlFor="location">Current Location:</label>
         <button onClick={getCurrentLocation} type='button' className='generate-location'>Get Current Location</button>
       </div>
-      <p className="p-ans1">Your current location is:<span className="ans-notbold"> {location.district} {location.state}</span></p>
+      <div className="flex-ans1">
+        <p className="p-ans1"> State:</p> <span className="span-margin">{location.state} </span><p className="p-ans1">District:</p><span>{location.district}</span>
+      </div>
+      <div className="weather-container">
+        <label htmlFor="weather">Current weather:</label>
+        <button onClick={getCurrentWeather} type='button' className='generate-weather'>Get Current weather</button>
+      </div>
+      <div className="flex-ans2">
+        <p className="p-ans2"> temperature(in ℃):</p> <span className="span-margin">{weather.temperature} </span><p className="p-ans2">humidity:</p><span>{weather.humidity}</span>
+      </div>
       <div className="select-container"> 
         <label htmlFor="seasonCrop">Select Season:</label>
         <select
@@ -81,8 +109,8 @@ const App = () => {
       <p className="p-ans2">The selected seasonal crop is:<span className="ans-notbold"> {seasonCrop}</span></p>
       <button className="form-submission" type="submit">Submit</button>
     </form>
-    </div>
-    
+    <div className="prediction-result"><p className="prediction-crop">Crop:</p><span className="ideal-crop">{data.crop}</span></div>
+    </div> 
   );
 };
 
